4cfd3feda9ac1c764973f14724d3bbe6f684548d
beddinao <edbilal916@gmail.com>
2024-11-11 21:56:29 +0100

    update

--- compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ generator.cpp lodepng/lodepng.cpp -o convert -I lodepng -D TERM_HEIGHT=$(tput lines) -D TERM_WIDTH=$(tput cols)
---
+++ convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "Usage ./convert.sh [path to png file]"
+	exit 1
+fi
+
+if [ -f $1 ];
+then
+	./compile
+	./a.out $1
+	rm a.out
+else
+	echo "file " $1 " does not exist"
+fi
